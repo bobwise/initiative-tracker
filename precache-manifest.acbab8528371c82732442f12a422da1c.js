a94d185d141acd7913f39afbce707bb9aafc6b45
Bob Wise <bobwise@quickenloans.com>
2020-03-04 15:15:09 -0500

    Updates

---
+++ precache-manifest.acbab8528371c82732442f12a422da1c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fe7a2435aa7888fc1acf89b1c120561",
+    "url": "/initiative-tracker/index.html"
+  },
+  {
+    "revision": "4db5438a970fd5ae84f6",
+    "url": "/initiative-tracker/static/css/main.e1b6df2a.chunk.css"
+  },
+  {
+    "revision": "b3a97ac6e77c4baffc57",
+    "url": "/initiative-tracker/static/js/2.9ac26ac3.chunk.js"
+  },
+  {
+    "revision": "4db5438a970fd5ae84f6",
+    "url": "/initiative-tracker/static/js/main.dbab5565.chunk.js"
+  },
+  {
+    "revision": "aa26c79c17f1b6604fa0",
+    "url": "/initiative-tracker/static/js/runtime~main.a0faf316.js"
+  }
+]);
